db89a74bbb39f941d9cf7b8649e45656e825cd68
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-04-10 13:14:51 +0200

    Add active articles JS

--- js/script.js
+++ js/script.js
@@ -9,16 +9,24 @@ document.getElementById('test-button').addEventListener('click', function(){
 
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
 
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  /* [IN PROGRESS] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('article.active');
 
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
